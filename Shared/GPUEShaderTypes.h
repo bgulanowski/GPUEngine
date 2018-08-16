16076671d287a3c17993aa3a3e2f62e49197bc43
Brent Gulanowski <bgulanowski@gmail.com>
2018-08-16 16:11:51 -0400

    Clean up old code. Update preprocessor guards.

--- Shared/GPUEShaderTypes.h
+++ Shared/GPUEShaderTypes.h
@@ -6,45 +6,10 @@
 //  Copyright © 2018 Lichen Labs. All rights reserved.
 //
 
-#ifndef ShaderTypes_h
-#define ShaderTypes_h
+#ifndef GPUEShaderTypes_h
+#define GPUEShaderTypes_h
 
 #import <simd/simd.h>
-
-#if 0
-typedef enum {
-    Simplex,
-    Voronoi,
-    Ridged,
-    // others as we figure them out
-} GeneratorType;
-
-typedef struct {
-    unsigned octaves;
-    float persistence;
-    vector_float3 offset; // TODO: remove in favour of Source.transform
-    vector_float2 scale; // TODO: remove in favour of Source.transform
-    // TODO: more parameters affecting other noise types
-} NoiseParams;
-
-typedef struct {
-    float scale;
-} NormalParams;
-
-typedef struct {
-    NoiseParams noise;
-    GeneratorType type;
-    matrix_float4x4 transform;
-} SourceParams;
-
-typedef struct {
-    vector_uint2 size;
-    vector_float3 offset;
-    vector_float2 scale;
-    float heightFactor;
-    float multiple; // the noise scale relative to the terrain scale
-} TerrainParams;
-#endif
 
 typedef struct {
     vector_float4 position;
@@ -58,4 +23,4 @@ typedef struct {
     vector_float2 uv;
 } ColourVertex;
 
-#endif /* ShaderTypes_h */
+#endif /* GPUEShaderTypes_h */
